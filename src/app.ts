e9bef14edfa20f47a7afa31419ff835e8152a0b0
oliver zulett <oliverzulett@gmail.com>
2021-08-13 00:54:57 -0400

    feat(settings): basic lint configuration

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import express from 'express';
 
 class App {
   public app: express.Application;
+
   constructor() {
     this.app = express();
-    this.app.use(express.json()); //recibe json
-    this.app.use(express.urlencoded({ extended: true })); //permite que los nested jsons
+    this.app.use(express.json()); // recibe json
+    this.app.use(express.urlencoded({ extended: true })); // permite que los nested jsons
     this.app.route('/status').get((req, res) => {
       res.status(200).send({ status: 'success' });
     });
