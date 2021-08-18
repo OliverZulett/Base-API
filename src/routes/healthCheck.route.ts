90481648c7b9b650e7ad3d86b5a602b271dbf953
oliver zulett <oliverzulett@gmail.com>
2021-08-17 21:51:06 -0400

    feat(logger): logger middleware implemented

--- src/routes/healthCheck.route.ts
+++ src/routes/healthCheck.route.ts
@@ -1,5 +1,6 @@
 import { HealthCheckController } from '../controllers/healthCheck/healthCheck.controller';
 import * as express from 'express';
+import { LoggerMiddleware } from '../middlewares/logger.middleware';
 
 export class HealthCheckRoute {
   private readonly healthCheckController: HealthCheckController;
@@ -9,6 +10,8 @@ export class HealthCheckRoute {
   }
 
   public routes(app: express.Application): void {
-    app.route('/healthCheck').get(this.healthCheckController.checkHealth);
+    app
+      .route('/healthCheck')
+      .get(LoggerMiddleware.logRequest, this.healthCheckController.checkHealth);
   }
 }
